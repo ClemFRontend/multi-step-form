832342d8f8f177b055faa61939787498460bc39f
clément perreaut <clement.perreaut@diiage.org>
2023-05-28 11:25:58 +0200

    🚧 Fix Web

--- utils/animations.js
+++ utils/animations.js
@@ -41,3 +41,56 @@ export const horizontalTransition = {
         };
     },
 };
+
+
+// Without react native reanimated -> for web
+export const horizontalTransitionWeb = {
+    gestureDirection: 'horizontal',
+    transitionSpec: {
+        open: {
+            animation: 'timing',
+            config: {
+                duration: 300,
+            },
+        },
+        close: {
+            animation: 'timing',
+            config: {
+                duration: 300,
+            },
+        },
+    },
+    cardStyleInterpolator: ({ current, next, layouts }) => {
+        return {
+            cardStyle: {
+                transform: [
+                    {
+                        translateX: current.progress.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [layouts.screen.width, 0],
+                            extrapolate: 'clamp',
+                        }),
+                    },
+                    {
+                        translateX: next
+                            ? next.progress.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [0, -layouts.screen.width],
+                                extrapolate: 'clamp',
+                            })
+                            : 0,
+                    },
+                ],
+            },
+            overlayStyle: {
+                opacity: current.progress.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 0.5],
+                    extrapolate: 'clamp',
+                }),
+            },
+        };
+    },
+};
+
+
